e2eb06f196d8c632d547583817a9e0bcd135a94e
Hoàng Ân Vũ <vuhoanganptit@gmail.com>
2026-01-20 09:45:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8197552441-8d4b4",
+  "appId": "1:41255619441:web:566882b13ce1e002ad84d0",
+  "apiKey": "AIzaSyCEd8UHfCQQ2hjD7oFMnZiHTdui9CShsaQ",
+  "authDomain": "studio-8197552441-8d4b4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "41255619441"
+};
